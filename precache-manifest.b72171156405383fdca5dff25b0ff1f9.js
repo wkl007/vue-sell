a8ecc762413704a583a13495bac6d57462c3a541
wkl007 <499657357@qq.com>
2020-02-11 03:34:56 +0000

    Deploying to gh-pages from master - 7e634a9a01f9d34e6454789bc0b1ac938ccde1cb 🚀

---
+++ precache-manifest.b72171156405383fdca5dff25b0ff1f9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46182f795e3ee1b357e",
+    "url": "/vue-sell/css/app.e12b9d21.css"
+  },
+  {
+    "revision": "f87444ae5e8a886eb25b",
+    "url": "/vue-sell/css/chunk-vendors.8ea961ec.css"
+  },
+  {
+    "revision": "7a13aae63527e0ec37c7e377dd4886a9",
+    "url": "/vue-sell/data.json"
+  },
+  {
+    "revision": "c5438fa90be5da041bbec3945fb82c00",
+    "url": "/vue-sell/favicon.png"
+  },
+  {
+    "revision": "c60c4d39f83feebe9d441d487e67336e",
+    "url": "/vue-sell/fonts/cubeic.c60c4d39.woff"
+  },
+  {
+    "revision": "dcc2b6f1e42abe71bc9f1a83f85f47d4",
+    "url": "/vue-sell/fonts/cubeic.dcc2b6f1.ttf"
+  },
+  {
+    "revision": "aa6d1a02a93b1b5516442dd3ee286212",
+    "url": "/vue-sell/img/brand@3x.aa6d1a02.png"
+  },
+  {
+    "revision": "98413c3c583bbbe8ad06c0bf49bca526",
+    "url": "/vue-sell/img/sell-icon.98413c3c.svg"
+  },
+  {
+    "revision": "f6fe99bcf2b7b28401887a40fb92b5a6",
+    "url": "/vue-sell/index.html"
+  },
+  {
+    "revision": "d46182f795e3ee1b357e",
+    "url": "/vue-sell/js/app.b543a276.js"
+  },
+  {
+    "revision": "f87444ae5e8a886eb25b",
+    "url": "/vue-sell/js/chunk-vendors.964fb2bb.js"
+  },
+  {
+    "revision": "e4b8967929c9a99022a344ec1c4d091f",
+    "url": "/vue-sell/js/chunk-vendors.964fb2bb.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6f1e299d252a904d93286fd76af8a0",
+    "url": "/vue-sell/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-sell/robots.txt"
+  }
+]);
